d1fea25c3a1e8be2fa69981c49d5b118c81045a6
ymk <ymg4798@naver.com>
2023-02-15 19:46:51 +0900

    DB 데이터 접근 1편 - Datasource 기본이론

--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -20,7 +20,7 @@ class MemberRepositoryV0Test {
     @Test
     void crud() throws SQLException {
         //save
-        Member member = new Member("memberV5", 10000);
+        Member member = new Member("memberV5", 1000);
         repository.save(member);
 
         //findById
